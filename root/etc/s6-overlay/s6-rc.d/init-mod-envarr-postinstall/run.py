b15390b58dafc0d26fa3fc27e98598c0f6e1e3d9
celsius narhwal <hello@celsiusnarhwal.dev>
2024-04-20 12:02:27 -0400

    Update mod

--- root/etc/s6-overlay/s6-rc.d/init-mod-envarr-postinstall/run.py
+++ root/etc/s6-overlay/s6-rc.d/init-mod-envarr-postinstall/run.py
@@ -3,6 +3,12 @@ from pathlib import Path
 
 import xmltodict
 
+CONFIG_FILE_PATH = Path("/config/config.xml")
+
+
+def msg(m: str):
+    return f"[Envarr] {m}"
+
 
 def get_app():
     apps = ["sonarr", "radarr", "lidarr", "readarr", "prowlarr"]
@@ -11,10 +17,23 @@ def get_app():
         return next((app for app in apps if Path(f"/app/{app}").exists()))
     except StopIteration:
         print(
-            f"Envarr is only compatible with LinuxServer.io's Sonarr, Radarr, Lidarr, Readarr, and Prowlarr images, "
-            f"so it will have no effect here."
+            msg(
+                f"Envarr is only compatible with LinuxServer.io's Sonarr, Radarr, Lidarr, Readarr, and "
+                f"Prowlarr images, so it will have no effect here."
+            )
         )
         exit(0)
+
+
+def check_config_file():
+    if not CONFIG_FILE_PATH.exists():
+        raise FileNotFoundError(msg(f"{CONFIG_FILE_PATH} does not exist."))
+
+    if not CONFIG_FILE_PATH.is_file():
+        raise IsADirectoryError(msg(f"{CONFIG_FILE_PATH} is a directory."))
+
+    if not os.access(CONFIG_FILE_PATH, os.W_OK):
+        raise PermissionError(msg(f"{CONFIG_FILE_PATH} is not writable."))
 
 
 def apply(app: str):
@@ -27,13 +46,19 @@ def apply(app: str):
 
         if env_value := os.getenv(env_name):
             config["Config"][setting] = env_value
-            print(f'From ENV {env_name}: {setting}="{env_value}"')
+            print(msg(f'From ENV {env_name}: {setting}="{env_value}"'))
 
     new_xml = xmltodict.unparse(config, pretty=True).splitlines(keepends=True)[1:]
     config_file.open("w").writelines(new_xml)
 
+    print(msg("Settings applied successfully."))
+
 
 if __name__ == "__main__":
     application = get_app()
-    print(f"Applying {application.capitalize()} settings from environment variables...")
+    print(
+        msg(
+            f"Applying {application.capitalize()} settings from environment variables..."
+        )
+    )
     apply(application)
